ad11b9525e74421d7e0bf9f918e4377cfe874bf4
freetools4u <swipetoconnect@gmail.com>
2026-07-23 16:52:10 +0530

    Update how-to-automate-your-entire-workday-using-ai.ts

--- src/articles/how-to-automate-your-entire-workday-using-ai.ts
+++ src/articles/how-to-automate-your-entire-workday-using-ai.ts
@@ -29,7 +29,7 @@ export const post: BlogPost = {
     twitter: 'zenirein',
     github: 'freetools4u'
   },
-  coverImage: 'images/ai-workday-atutomation.png',
+  coverImage: 'https://blog.zenire.in/images/ai-workday-atutomation.png',
   keywords: [
     'How to automate workday with AI',
     'AI workday automation blueprint',
